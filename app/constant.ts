bfa252a11acb2b5d4f17c5241f21029564e2b1cb
HotWasabiSushi <76900409+CallMeWasabi@users.noreply.github.com>
2024-01-04 00:39:06 +0700

    change clientWebserverUrl

--- app/constant.ts
+++ app/constant.ts
@@ -1,2 +1,2 @@
-export const clientWebserverUrl = "https://7712-101-108-69-171.ngrok-free.app"
+export const clientWebserverUrl = "https://a760-101-108-69-171.ngrok-free.app"
 export const shopUrl = "https://pordeejai-esmn.vercel.app"
